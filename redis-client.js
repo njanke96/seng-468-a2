78efaf988945606ac7d010fb36757bf4168827e4
Nathan Janke <nathanjanke96@gmail.com>
2022-03-23 13:24:59 -0700

    pages

--- redis-client.js
+++ redis-client.js
@@ -1,10 +1,10 @@
 const redis = require('redis');
-const {promisify} = require('util');
-const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient({
+  url: process.env.REDIS_URL
+});
+
+client.on('error', (err) => console.error('Redis Client Error', err));
 
 module.exports = {
   ...client,
-  getAsync: promisify(client.get).bind(client),
-  setAsync: promisify(client.set).bind(client),
-  keysAsync: promisify(client.keys).bind(client)
-};
+}
--- server.js
+++ server.js
@@ -2,24 +2,14 @@ const express = require('express');
 const app = express();
 
 
-const redisClient = require('./redis-client');
-
-app.get('/store/:key', async (req, res) => {
-  const { key } = req.params;
-  const value = req.query;
-  await redisClient.setAsync(key, JSON.stringify(value));
-  return res.send('Success');
-});
-
-app.get('/:key', async (req, res) => {
-  const { key } = req.params;
-  const rawData = await redisClient.getAsync(key);
-  return res.json(JSON.parse(rawData));
-});
+const redis = require('./redis-client');
 
 app.get('/', (req, res) => {
   return res.send('Hello world');
 });
+
+// everything in /public should just be served
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
